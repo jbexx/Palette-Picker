c8afe226b70350fff5802d718a708fbef5f3bd08
Jonathan Beckman <jbecks.media@gmail.com>
2017-10-04 15:07:18 -0600

    complete post functionality to the palette database

--- server.js
+++ server.js
@@ -18,12 +18,29 @@ app.post('/api/v1/projects/', (request, response) => {
 
     database('projects').insert(project_name, '*')
     .then( project => {
-        response.status(201).json( project )
+        response.status(201).json(project)
     })
-    .catch(error => {
+    .catch( error => {
         response.status(500).json({ error });
     })
 })
+
+app.post('/api/v1/palettes/', (request, response) => {
+    const { name, hex1, hex2, hex3, hex4, hex5, project_id } = request.body
+
+    database('palettes').insert({ name, hex1, hex2, hex3, hex4, hex5, project_id }, '*')
+    .then( palette => {
+        response.status(201).json(palette)
+    })
+    .catch( error => {
+        response.status(500).json({ error });
+    })
+})
+
+app.get('/api/v1/projects', (request, response) => {
+    database('projects').select()
+    .then
+});
 
 
 app.listen(app.get('port'), () => {
